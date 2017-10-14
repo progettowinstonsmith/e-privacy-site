996e3296283c363bb2c6e452bc53430bfe4c1114
Emmanuele Somma <exedre@gmail.com>
2017-10-14 11:07:16 +0200

    step

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 LOAD_CONTENT_CACHE = False
 # CACHE_MODIFIED_METHOD = 'md5'
 
-COUNTDOWN = True
+COUNTDOWN = False
 EVENTBRITE_EID="""
 <div style="width:195px; text-align:center;" ><iframe  src="https://www.eventbrite.it/countdown-widget?eid=37526459651" frameborder="0" height="544" width="195" marginheight="0" marginwidth="0" scrolling="no" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:195px; text-align:center;" ><a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="http://www.eventbrite.it/">Con tecnologia Eventbrite</a></div></div>
 """
