bb88fa4681970b09db0f2d322d757f5d8d953382
Emmanuele Somma <exedre@gmail.com>
2026-09-08 18:37:07 +0200

    step

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,21 +10,21 @@ from __future__ import unicode_literals
 
 ##### Configurazioni per il sync 
 
-EDITION = 37
+EDITION = 39
 RELAZIONI = 'talks'
 RELATORI = 'curricula'
-EPRIVACY_N = 'XXXVIII'
-SESSIONI = '1M,1P'.split(',')
+EPRIVACY_N = 'XXXIX'
+SESSIONI = '1M,1P,2M'.split(',')
 ORGANIZZATORI = ['calamari', 'giorio', 'somma', 'berto', 'priolo', 'smith']
-EVENT_PATH = 'content/2026/summer/'
+EVENT_PATH = 'content/2026/autumn/'
 YEAR = '2026'
-EDITION = 'summer'
-SITENAME = 'e-privacy 2026 summer: Privacy senza Garante o Garante senza Privacy?'
-IMAGE = 'e-privacy-XXXVIII.png'
+EDITION = 'autumn'
+SITENAME = 'e-privacy 2026 autumn: Se l’IA fa i tuoi compiti, chi fa i compiti dell’IA?'
+IMAGE = 'e-privacy-XXXIX.png'
 PROPOSALS_OPEN = False
 
 COUNTDOWN = False
-EVENT_TIME = '2026/04/24 09:30'
+EVENT_TIME = '2026/11/13 09:30'
 
 LIVE_AT=False
 # "15:00"
@@ -37,10 +37,10 @@ LIVE_URL="https://www.youtube.com/watch?v=m58flx5d1qI"
 ### CONFIGURAZIONE BARRA LATERALE
 
 LATERAL_BAR = [
-    ('Il programma', f'/e-privacy-{EPRIVACY_N}.html'),
+    # [ ] ('Il programma', f'/e-privacy-{EPRIVACY_N}.html'),
     # [ ] ('Consegna Slides', '/consegna-slides.html'),
-    # [ ] ('Call for Paper', f'/e-privacy-{EPRIVACY_N}-cfp.html'),
-    # [ ] ('Come arrivare', f'/e-privacy-{EPRIVACY_N}-arrivare.html'),
+    ('Call for Paper', f'/e-privacy-{EPRIVACY_N}-cfp.html'),
+    ('Come arrivare', f'/e-privacy-{EPRIVACY_N}-arrivare.html'),
     ('Mappa delle edizioni', '/mappa-edizioni-e-privacy.html'),
     ('Donazioni', '/donazioni-e-privacy.html'),
     ('Collabora', '/collabora.html'),
